57e46eb94250242ed20ddecd7fb5832ed6655ccc
Ville Einiö <ville.einio@gmail.com>
2022-03-18 22:35:51 +0200

    Added absolute- and string-values

--- vw_weconnect_id/data_providers/vehicle_data_property.py
+++ vw_weconnect_id/data_providers/vehicle_data_property.py
@@ -1,15 +1,18 @@
 from datetime import datetime
 import logging
 from vw_weconnect_id.tools.weconnect_logger import WeConnectCSVLogger, WeConnectLoggerError
+from enum import Enum
 
 
 class WeconnectVehicleDataProperty:
-    def __init__(self, name, value, desc, category, unit=None, log_data=False) -> None:
-        self.__name = name
-        self.__value = str(value)
+    def __init__(self, data_id, value, desc, category, unit=None, log_data=False) -> None:
+        self.__data_id = data_id
+        self.__string_value = str(value.value if isinstance(value, Enum) else value)
+        self.__absolute_value = value
         self.__unit = unit
         self.__desc = desc
         self.__category = category
+        self.__call_on_update = []
         self.__time_updated = datetime.now().time().strftime("%H.%M:%S")
         self.__date_updated = datetime.now().date().strftime("%d.%m.%Y")
         self.__log_data = log_data
@@ -24,19 +27,28 @@ class WeconnectVehicleDataProperty:
         return self.__desc
 
     @property
-    def name(self) -> str:
-        return self.__name
+    def data_id(self) -> str:
+        return self.__data_id
 
     @property
-    def value(self) -> str:
-        return self.__value
+    def string_value(self) -> str:
+        return self.__string_value
 
-    @value.setter
-    def value(self, value) -> None:
-        self.__value = str(value)
+    @property
+    def absolute_value(self):
+        return self.__absolute_value
+
+    def update_value(self, value) -> None:
+        self.__string_value = str(value.value if isinstance(value, Enum) else value)
+        self.__absolute_value = value
+        for function in self.__call_on_update:
+            function()
         self.__time_updated = datetime.now().time().strftime("%H.%M:%S")
         self.__date_updated = datetime.now().date().strftime("%d.%m.%Y")
         self.__log()
+
+    def add_callback_function(self, function: callable) -> None:
+        self.__call_on_update.append(function)
 
     def __log(self) -> None:
         if not self.__log_data:
@@ -49,9 +61,9 @@ class WeconnectVehicleDataProperty:
     def data_as_tuple(self) -> tuple:
         return (
             (
-                f"{self.__value}{self.__unit}"
+                f"{self.__string_value}{self.__unit}"
                 if self.__unit is not None
-                else self.__value
+                else self.__string_value
             ),
             self.__time_updated,
             self.__date_updated,
@@ -59,7 +71,7 @@ class WeconnectVehicleDataProperty:
 
     def __str__(self) -> str:
         return (
-            f"{self.__desc:<50}{self.__value}"
+            f"{self.__desc:<50}{self.__string_value}"
             if self.__unit is None
-            else f"{self.__desc:<50}{self.__value}{self.__unit}"
+            else f"{self.__desc:<50}{self.__string_value}{self.__unit}"
         )
