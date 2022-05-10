a02ae221cec1da4e5f9ca05fb0dc480e285fd834
Ville Einiö <ville.einio@gmail.com>
2022-05-10 21:34:34 +0300

    Initial commit

--- build_tools/loggers_configurator.py
+++ build_tools/loggers_configurator.py
@@ -2,7 +2,7 @@ import logging
 
 
 def load_loggers(config: dict) -> None:
-    logs = config["paths"]["logs"]
+    logs_path = config["paths"]["application_logs"]
 
     formatter = logging.Formatter(
         fmt="%(levelname)s | %(asctime)s.%(msecs)03d | %(message)s",
@@ -13,25 +13,30 @@ def load_loggers(config: dict) -> None:
         datefmt="%d.%m.%Y | %H:%M:%S",
     )
 
-    file_handler_all = logging.FileHandler(logs + "all.log")
+    file_handler_all = logging.FileHandler(logs_path + "all.log")
     file_handler_all.setFormatter(formatter_all)
+
+    file_handler_exceptions = logging.FileHandler(logs_path + "exceptions.log")
+    file_handler_exceptions.setLevel(logging.ERROR)
+    file_handler_exceptions.setFormatter(formatter_all)
 
     logger_names = [
         "main",
         "vehicle",
         "climate",
-        "exception",
         "led",
-        "weconnect updater",
+        "weconnect_updater",
         "display",
         "button",
         "datalogger",
-        "data property"
+        "data_properties",
+        "scene_controller"
     ]
     for logger_name in logger_names:
         logger = logging.getLogger(logger_name)
         logger.setLevel(logging.DEBUG)
-        file_handler = logging.FileHandler(logs + logger_name + ".log")
+        file_handler = logging.FileHandler(logs_path + logger_name + ".log")
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
         logger.addHandler(file_handler_all)
+        logger.addHandler(file_handler_exceptions)
