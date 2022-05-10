4aeb05a3f61026f455b05e928ad139baa41a2eae
Ville Einiö <ville.einio@gmail.com>
2022-05-10 14:21:42 +0300

    Removed useless class and added logging

--- vw_weconnect_id/tools/weconnect_logger.py
+++ vw_weconnect_id/tools/weconnect_logger.py
@@ -1,58 +1,47 @@
 import csv
 from pathlib import Path
-from config_loader import ConfigLoader
 import os
 import logging
 
 
 class WeConnectLoggerError(Exception):
-    def __init__(self, message: str) -> None:
-        super().__init__(message)
-        self.message = message
+    pass
 
 
-class WeConnectCSVLogger:
-    def log(data) -> None:
-        '''
-        Logs data from WeConnectVehicleDataProperty-object to csv file in path given in config.json
+logger = logging.getLogger("datalogger")
 
-        Args:
-            data (WeConnectVehicleDataProperty): [WeConnectVehicleDataProperty-object where data is logged from]
 
-        Raises:
-            WeConnectLoggerError: [Raised if creating dir to given path fails]
-            WeConnectLoggerError: [Raised if something fails when writing to .csv-file]
-        '''
-        logging.info(f"Logging data from property '{data.data_id}'")
-        dir_path = Path(
-            ConfigLoader.load_config()["paths"]["data"] + f"/{data.category}"
-        )
-        if not dir_path.is_dir():
-            try:
-                logging.info(f"Creating new folder to {dir_path}")
-                os.mkdir(dir_path)
-            except OSError as e:
-                logging.error(
-                    f"Error occurred while trying to create directory in path {dir_path}\n{e}"
-                )
-                raise WeConnectLoggerError(
-                    f"Error occurred while trying to create directory in path {dir_path}\n{e}"
-                )
-        
-        file_path = Path(str(dir_path) + f"/{data.data_id.replace(' ', '_')}.csv")
-        exists = file_path.is_file()
-        
+def log(data_property) -> None:
+    """
+    Logs data from WeConnectVehicleDataProperty-object to csv file in given path
+
+    Args:
+        data (WeConnectVehicleDataProperty): [WeConnectVehicleDataProperty-object where data is logged from]
+
+    Raises:
+        WeConnectLoggerError: [Raised if creating dir to given path fails or if writing to file fails]
+    """
+    path = data_property.logger_path
+    logger.info(f"Logging data from property '{data_property.data_id}'")
+    dir_path = Path(path + f"/{data_property.category}")
+    if not dir_path.is_dir():
         try:
-            with open(file_path, "a") as csv_file:
-                writer = csv.writer(csv_file, delimiter=";")
-                if not exists:
-                    writer.writerow(("value", "time", "date"))
-                logging.info(f"Writing data to {file_path}")
-                writer.writerow(data.data_as_tuple())
-        except Exception as e:
-            logging.error(
-                f"Error occurred while trying to log data into file in path {file_path}\n{e}"
-            )
-            raise WeConnectLoggerError(
-                f"Error occurred while trying to log data into file in path {file_path}\n{e}"
-            )
+            logger.info(f"Creating new folder to path {dir_path}")
+            os.mkdir(dir_path)
+        except OSError as e:
+            logger.exception(e)
+            raise WeConnectLoggerError(f"Error occurred while trying to create directory in path {dir_path}")
+
+    file_path = Path(str(dir_path) + f"/{data_property.data_id.replace(' ', '_')}.csv")
+    exists = file_path.is_file()
+
+    try:
+        with open(file_path, "a") as csv_file:
+            writer = csv.writer(csv_file, delimiter=";")
+            if not exists:
+                writer.writerow(("value", "time", "date"))
+            logger.info(f"Writing data to {file_path}")
+            writer.writerow(data_property.get_logging_data())
+    except Exception as e:
+        logger.exception(e)
+        raise WeConnectLoggerError(f"Error occurred while trying to log data into file in path {file_path}")
