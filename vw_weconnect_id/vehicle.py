9fea1512dec2049f2a3775b4e438f22e99375f50
Ville Einiö <ville.einio@gmail.com>
2022-05-10 14:43:32 +0300

    Added support for adding translations to data properties. Added logging. Added data property logging path configuration

--- vw_weconnect_id/vehicle.py
+++ vw_weconnect_id/vehicle.py
@@ -1,6 +1,5 @@
 import logging
 from weconnect.elements.vehicle import Vehicle
-from led_tools.led_controller import IndicatorLEDController
 from vw_weconnect_id.controllers.climate_controller import ClimateController
 from vw_weconnect_id.data_providers.battery_data import WeconnectBatteryData
 from vw_weconnect_id.data_providers.climatisation_data import WeconnectClimateData
@@ -9,16 +8,17 @@ from vw_weconnect_id.data_providers.vehicle_data_property import WeconnectVehicl
 from vw_weconnect_id.tools.weconnect_updater import WeConnectUpdater
 
 
+logger = logging.getLogger("vehicle")
+
+
 class VolkswagenIdVehicle:
 
     CAR_BRANDS = {"WCAR": "Volkswagen"}
 
-    def __init__(self, vehicle: Vehicle, updater: WeConnectUpdater, led_controller: IndicatorLEDController) -> None:
-        
+    def __init__(self, vehicle: Vehicle, updater: WeConnectUpdater, config: dict) -> None:
+        logger.debug("Initializing vehicle")
         self.__import_vehicle_properties(vehicle)
         self.__setup_vehicle()
-
-        self.__climate_controller = None
 
         self.__battery_data_provider = WeconnectBatteryData(vehicle)
         self.__climatisation_data_provider = WeconnectClimateData(vehicle)
@@ -27,48 +27,75 @@ class VolkswagenIdVehicle:
         self.__data = {}
         self.__import_vehicle_data()
         
-        led_controller.setup_vehicle_dependent_leddrivers(self)
+        self.__add_data_property_translations(config=config)
+        self.__add_data_property_logger_paths(config=config)
         
-        self.__climate_controller = ClimateController(vehicle=self, updater=updater, led_controller=led_controller)
+        self.__climate_controller = ClimateController(vehicle=self, updater=updater)
 
     def __import_vehicle_properties(self, vehicle: Vehicle) -> None:
-        logging.info("Importing vehicle properties")
+        logger.debug("Importing vehicle properties")
         self.__weconnect_vehicle = vehicle
         self.__brand = self.CAR_BRANDS[vehicle.devicePlatform.value.value]
         self.__model = vehicle.model
         self.__vin = vehicle.vin
 
     def __setup_vehicle(self) -> None:
-        logging.info("Enabling request tracker for climatisation control")
+        logger.debug("Enabling request tracker for climatisation controller")
         self.__weconnect_vehicle.enableTracker()
 
     def __import_vehicle_data(self) -> None:
-        logging.info("Importing vehicle data")
+        logger.debug("Importing vehicle data")
         self.__data.update(self.__battery_data_provider.get_data())
         self.__data.update(self.__climatisation_data_provider.get_data())
         self.__data.update(self.__readiness_data_provider.get_data())
+
+    def __add_data_property_translations(self, config: dict) -> None:
+        logger.debug("Adding translations for data properties")
+        for data_id, translations in config["translations"]:
+            self.get_data_property(data_id=data_id).add_translations(translations=translations)
+
+    def __add_data_property_logger_paths(self, config: dict) -> None:
+        logger.debug("Setting logging path for data properties that log their data")
+        for data_property in self.__data.values():
+            if data_property.logging_enabled:
+                data_property.set_logger_path(config["paths"]["data_logs"])
 
     def start_climate_control(self) -> None:
         if self.__climate_controller is None:
             return
         try:
-            self.__climate_controller.start(call_when_ready=lambda x: print(x))
+            logger.info("Starting vehicle climate control")
+            self.__climate_controller.start()
         except Exception as e:
-            logging.error(e)
+            logger.exception(e)
 
     def stop_climate_control(self) -> None:
         if self.__climate_controller is None:
             return
         try:
-            self.__climate_controller.stop(call_when_ready=lambda x: print(x))
+            logger.info("Stopping vehicle climate control")
+            self.__climate_controller.stop()
         except Exception as e:
-            logging.error(e)
+            logger.exception(e)
 
-    def get_data_property(self, data_id) -> WeconnectVehicleDataProperty:
+    def get_data_property(self, data_id: str) -> WeconnectVehicleDataProperty:
         return self.__data[data_id]
 
-    def add_callback_function(self, data_id, function: callable) -> None:
+    def add_callback_function(self, data_id: str, function: callable) -> None:
+        logger.info(f"Adding callback function {function.__name__} to data property ID {data_id}")
         self.get_data_property(data_id).add_callback_function(function)
 
     def get_weconnect_vehicle(self) -> Vehicle:
         return self.__weconnect_vehicle
+
+    @property
+    def vin(self) -> str:
+        return self.__vin
+    
+    @property
+    def model(self) -> str:
+        return self.__model
+    
+    @property
+    def brand(self) -> str:
+        return self.__brand
