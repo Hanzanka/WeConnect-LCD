ef8aeceef7bc778937cd0e43dfeaf318cd47c58b
Ville Einiö <ville.einio@gmail.com>
2022-05-10 21:44:00 +0300

    Removed general exception for being useless

--- general_exception.py
+++
@@ -1,8 +0,0 @@
-class GeneralException(Exception):
-    def __init__(self, message: str, fatal: bool) -> None:
-        super().__init__()
-        self.message = message
-        self.fatal = fatal
-        
-    def __str__(self) -> str:
-        return self.message
