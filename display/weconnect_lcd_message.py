adb8b7741522550f6c572d265e0e59663f55139d
Ville Einiö <ville.einio@gmail.com>
2022-05-16 21:11:23 +0300

    Added logging

--- display/weconnect_lcd_message.py
+++ display/weconnect_lcd_message.py
@@ -3,19 +3,28 @@ from display.lcd_controller import LCDController
 import logging
 
 
+logger = logging.getLogger("lcd_message")
+
+
 def configure_auto_messages(
     config: dict, vehicle: VolkswagenIdVehicle, lcd_controller: LCDController
 ) -> None:
-    logging.info("Initializing automated messages")
+    logger.debug("Initializing WeConnectLCDMessages")
     for message_id in config["automated messages"].keys():
         WeConnectLCDMessage(config, message_id, vehicle, lcd_controller)
 
 
 class WeConnectLCDMessage:
     def __init__(
-        self, config: dict, message_id, vehicle: VolkswagenIdVehicle, lcd_controller: LCDController
+        self,
+        config: dict,
+        message_id,
+        vehicle: VolkswagenIdVehicle,
+        lcd_controller: LCDController,
     ) -> None:
+        logger.debug(f"Initializing WeConnectLCDMessage (ID: {message_id})")
         message_config = config["automated messages"][message_id]
+        self.__id = message_id
         self.__lcd_controller = lcd_controller
         self.__selective_messages = "trigger" in message_config
         if self.__selective_messages:
@@ -23,7 +32,9 @@ class WeConnectLCDMessage:
         self.__message_base = message_config["message base"]
         self.__message_time = message_config["time"]
         self.__translate = message_config["translate"]
-        self.__data_provider = vehicle.get_data_property(message_config["data provider id"])
+        self.__data_provider = vehicle.get_data_property(
+            message_config["data provider id"]
+        )
         self.__data_provider.add_callback_function(self.on_data_update)
 
     def on_data_update(self) -> None:
@@ -35,7 +46,11 @@ class WeConnectLCDMessage:
         self.__display_message(value)
 
     def __display_message(self, value) -> None:
+        message_content = self.__message_base.replace("{value}", value)
+        logger.debug(
+            f"Queueing WeConnectLCDMessage (ID: {self.__id}) with content ({message_content})"
+        )
         self.__lcd_controller.display_message(
-            message=self.__message_base.replace("{value}", value),
+            message=message_content,
             time_on_screen=self.__message_time,
         )
