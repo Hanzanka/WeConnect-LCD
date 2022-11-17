2ef1be25c7daf114e5c5a218c3398f86b9a7d616
Ville Einiö <ville.einio@gmail.com>
2022-11-17 19:53:39 +0200

    Added support for status bar

--- display/lcd_scene_controller.py
+++ display/lcd_scene_controller.py
@@ -1,17 +1,14 @@
-import logging
 from display.lcd_controller import LCDController
 from .lcd_scene import LCDScene
 from threading import Thread
-
-
-LOG = logging.getLogger("lcd_scene_controller")
+from .lcd_status_bar import LCDStatusBar
 
 
 class LCDSceneController:
     def __init__(self) -> None:
-        LOG.debug("Initializing LCDSceneController")
         self.__home_scene = None
         self.__selected_scene = None
+        self.__status_bar = None
         self.__last_scenes = {}
         self.__lcd_controller = LCDController(self)
 
@@ -19,31 +16,39 @@ class LCDSceneController:
     def lcd_controller(self) -> LCDController:
         return self.__lcd_controller
 
+    def set_status_bar(self, status_bar: LCDStatusBar) -> None:
+        self.__status_bar = status_bar
+
+    def update_status_bar(self) -> None:
+        if self.__selected_scene is not None:
+            self.refresh(self.__selected_scene)
+
     def set_home_scene(self, scene: LCDScene) -> None:
-        LOG.info(f"Added new home scene (ID: {scene.id})")
         self.__home_scene = scene
 
     def home(self) -> None:
-        LOG.info(f"Loading home scene (ID: {self.__home_scene.id})")
         self.__last_scenes.clear()
         self.load_scene(self.__home_scene)
 
     def restore_last_view(self) -> None:
-        LOG.debug("Restoring last LCDScene")
         if self.__selected_scene is not None:
             self.refresh(self.__selected_scene)
 
     def refresh(self, scene) -> None:
         if self.__selected_scene.id == scene.id:
+            if scene.has_title:
+                scene_content = scene.content
+                icons = self.__status_bar.icons
+                scene_content[0] = scene_content[0][: -len(icons)] + icons
+                self.__lcd_controller.update_lcd(scene_content)
+                return
             self.__lcd_controller.update_lcd(scene.content)
 
     def load_scene(self, scene: LCDScene) -> None:
-        LOG.debug(f"Loading LCDScene (ID: {scene.id})")
         self.__selected_scene = scene
         scene.load()
 
     def add_scene(self, scene: LCDScene) -> None:
-        LOG.debug(f"Adding new LCDScene (ID: {scene.id}) to LCDSceneController")
         self.__scenes.append(scene)
 
     def back(self) -> None:
@@ -56,8 +61,6 @@ class LCDSceneController:
         except KeyError:
             return
 
-        LOG.debug(f"Returning to last LCDScene (ID: {target_scene.id})")
-
         self.__last_scenes.pop(self.__selected_scene, None)
         self.__selected_scene.exit()
 
@@ -65,7 +68,6 @@ class LCDSceneController:
         self.__selected_scene.load()
 
     def __load_next_scene(self, scene: LCDScene) -> None:
-        LOG.info(f"Loading next LCDScene (ID: {scene.id})")
         self.__last_scenes[scene] = self.__selected_scene
         self.__selected_scene.exit()
 
@@ -77,7 +79,7 @@ class LCDSceneController:
             return
 
         target = self.__selected_scene.next
-        
+
         if target is None:
             return
 
