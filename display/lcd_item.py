1fb7132733808738f3c377edd026ad718817096e
Ville Einiö <ville.einio@gmail.com>
2022-11-21 20:19:18 +0200

    Added second titles to be displayed on the right side of the screen when content centering is off

--- display/lcd_item.py
+++ display/lcd_item.py
@@ -6,44 +6,71 @@ LOG = logging.getLogger("lcd_item")
 
 class LCDItem:
     def __init__(
-        self, title, id, target=None, target_args=None, content_centering=True
+        self,
+        id,
+        title,
+        second_title=None,
+        target=None,
+        target_args=None,
+        content_centering=True,
     ) -> None:
-        LOG.debug(f"Initializing LCDItem (ID: {id})")
-        self._scenes = []
-        self._content = ""
         self._id = id
         self._title = title
+        self._second_title = second_title
         self.__target = target
         self.__target_args = target_args
         self.__content_centering = content_centering
+        self._scenes = []
         self._selected = False
-        if self._title is not None:
-            self._update_content()
+        if title is not None:
+            self.unselect()
 
-    def _update_content(self) -> None:
+    def update_content(self, title=None, second_title=None) -> None:
+        if title is not None:
+            self._title = title
+        if second_title is not None:
+            self._second_title = second_title
         if self._selected:
+            self.select()
+        else:
+            self.unselect()
+        for scene in self._scenes:
+            scene.update()
+
+    def select(self) -> None:
+        self._selected = True
+        if (
+            self._second_title is None
+            or self._second_title is not None
+            and self.__content_centering
+        ):
             self._content = (
                 f">{self._title}".center(20)
                 if self.__content_centering
                 else f">{self._title}"
             )
-            return
-        self._content = (
-            self._title.center(20) if self.__content_centering else self._title
-        )
-
-    def select(self) -> None:
-        LOG.debug(f"Selected LCDItem (ID: {self._id})")
-        self._selected = True
-        self._update_content()
+        else:
+            self._content = (
+                f">{self._title}{self._second_title:>{19 - len(self._title)}}"
+            )
 
     def unselect(self) -> None:
         LOG.debug(f"Unselected LCDItem (ID: {self._id})")
         self._selected = False
-        self._update_content()
+        if (
+            self._second_title is None
+            or self._second_title is not None
+            and self.__content_centering
+        ):
+            self._content = (
+                self._title.center(20) if self.__content_centering else self._title
+            )
+        else:
+            self._content = (
+                f"{self._title}{self._second_title:>{20 - len(self._title)}}"
+            )
 
     def add_scene(self, scene) -> None:
-        LOG.debug(f"Adding LCDScene (ID: {scene.id}) to LCDItem (ID: {self._id})")
         self._scenes.append(scene)
 
     @property
