ed3f2e502de2b9cd94a92e61a2f6ff25caef8147
Ville Einiö <ville.einio@gmail.com>
2022-11-21 20:23:08 +0200

    Removed useless logging and renamed function

--- display/weconnect_lcd_item.py
+++ display/weconnect_lcd_item.py
@@ -1,11 +1,7 @@
-import logging
 from weconnect_id.data_providers.vehicle_data_property import (
     WeConnectVehicleDataProperty,
 )
 from display.lcd_item import LCDItem
-
-
-LOG = logging.getLogger("lcd_item")
 
 
 class WeConnectLCDItem(LCDItem):
@@ -21,17 +17,16 @@ class WeConnectLCDItem(LCDItem):
         self.__translate = translate
         self.__data_provider = data_provider
         self.__data_provider.add_callback_function(self.on_data_update)
-        self._update_content()
+        self.update_content()
 
-    def _update_content(self) -> None:
+    def update_content(self) -> None:
         value = self.__data_provider.custom_value_format(
             translate=self.__translate, include_unit=True
         )
-        indentation = 20 - len(self._title)
-        self._content = f"{self._title}{value:>{indentation}}"
+        self._content = f"{self._title}{value:>{20 - len(self._title)}}"
+
 
     def on_data_update(self) -> None:
-        LOG.debug(f"Updating WeConnectLCDItem (ID: {self._id}) value")
-        self._update_content()
+        self.update_content()
         for scene in self._scenes:
             scene.update()
