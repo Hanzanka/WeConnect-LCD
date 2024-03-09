adec29f0417d0f0084d1b79f0f215264627fe445
Ville <ville.einio@gmail.com>
2024-03-09 23:55:11 +0200

    Fix: actually a potential fix for WeConnectUpdater not responding

--- weconnect_id/tools/updater.py
+++ weconnect_id/tools/updater.py
@@ -5,6 +5,7 @@ from apscheduler.jobstores.base import ConflictingIdError
 import logging
 from weconnect.domain import Domain
 from led.led_driver import create_led_driver, LEDDriver
+import os
 
 
 LOG = logging.getLogger("weconnect_updater")
@@ -34,6 +35,10 @@ class WeConnectUpdater:
             default_frequency=10,
         )
 
+        self.__base_job_running = {}
+        self.__executions_skipped = 0
+        self.__job_restarts = 0
+
         self.__scheduler = BackgroundScheduler(timezone="Europe/Helsinki")
         self.__scheduler.start()
 
@@ -46,7 +51,12 @@ class WeConnectUpdater:
         self.__start_total_update_scheduler()
 
     def add_scheduler(
-        self, id: str, domains: list, interval: int, silent: bool, run_immediately: bool = True
+        self,
+        id: str,
+        domains: list,
+        interval: int,
+        silent: bool,
+        run_immediately: bool = True,
     ) -> None:
         LOG.debug(f"Adding WeConnectUpdater scheduler (ID: {id})")
         try:
@@ -72,8 +82,14 @@ class WeConnectUpdater:
             LOG.exception(f"WeConnectUpdater scheduler (ID: {id}) doesn't exist")
             raise e
 
-    def update(self, domains: list, silent: bool = False) -> None:
+    def update(self, domains: list, silent: bool = False, job_id: str = None) -> None:
         LOG.debug(f"Updating WeConnect data (Domains: {domains})")
+        if job_id is not None:
+            if self.__base_job_running[job_id]:
+                self.__on_max_instances_reached(job_id=job_id)
+                return
+            self.__base_job_running[job_id] = True
+
         if not silent:
             self.__update_led.blink()
 
@@ -88,6 +104,10 @@ class WeConnectUpdater:
 
         except Exception as e:
             LOG.exception(e)
+
+            if job_id is not None:
+                self.__base_job_running[job_id] = False
+
             self.__scheduler.pause()
             Timer(function=self.__scheduler.resume, interval=60).start()
             self.__update_led.turn_on()
@@ -98,28 +118,58 @@ class WeConnectUpdater:
 
         if self.__scheduler.state == 2:
             self.__scheduler.resume()
-            
+
+        if job_id is not None:
+            self.__base_job_running[job_id] = False
+
         LOG.debug(f"Successfully updated WeConnect data (Domains: {domains})")
 
     def __start_main_update_scheduler(self) -> None:
+        self.__base_job_running["MAIN_UPDATE_SCHEDULER"] = False
         self.__scheduler.add_job(
             id="MAIN_UPDATE_SCHEDULER",
             func=self.update,
-            args=[self.DOMAINS, self.__silent_main_update],
+            args=[self.DOMAINS, self.__silent_main_update, "MAIN_UPDATE_SCHEDULER"],
             trigger="interval",
             seconds=self.__update_rate,
-            replace_existing=True
+            max_instances=999,
+            replace_existing=True,
         )
 
     def __start_total_update_scheduler(self) -> None:
+        self.__base_job_running["TOTALUPDATE"] = False
         self.__scheduler.add_job(
             id="TOTALUPDATE",
             func=self.update,
-            args=[[Domain.ALL], self.__silent_main_update],
+            args=[[Domain.ALL], self.__silent_main_update, "TOTALUPDATE"],
             trigger="interval",
             hours=1,
-            replace_existing=True
+            max_instances=999,
+            replace_existing=True,
         )
+
+    def __on_max_instances_reached(self, job_id: str) -> None:
+        self.__executions_skipped += 1
+        LOG.error(
+            f"Other WeConnectUpdater job with (ID: {job_id})"
+            + f" was running (Skipped executions: {self.__executions_skipped})"
+            + f" (Job restarts: {self.__job_restarts})"
+        )
+        if self.__executions_skipped >= 5:
+            LOG.info(
+                "Restarting WeConnectUpdater jobs"
+            )
+            self.__scheduler.remove_job(job_id="MAIN_UPDATE_SCHEDULER")
+            self.__scheduler.remove_job(job_id="TOTALUPDATE")
+            self.__start_main_update_scheduler()
+            self.__start_total_update_scheduler()
+
+            self.__executions_skipped = 0
+            self.__job_restarts += 1
+
+        if self.__job_restarts >= 5:
+            LOG.info("Rebooting system due too many restarted jobs")
+            os.system("sudo reboot")
 
     @property
     def weconnect(self) -> WeConnect:
